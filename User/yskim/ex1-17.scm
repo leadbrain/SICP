368ca6acb09c847a7f8b49da0b3e13f35a500965
leadbrain <leadbrain@gmail.com>
2013-05-10 09:33:06 +0900

    문제풀이

---
+++ User/yskim/ex1-17.scm
@@ -0,0 +1,21 @@
+(define (* a b)
+  (cond ((= b 0)
+         0)
+        ((even? b)
+         (* (double a) (halve b)))
+        (else 
+         (+ a (* a (- b 1))))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (double a)
+  (+ a a))
+
+(define (halve b)
+  (/ b 2))
+
+(* 1 2)
+(* 3 4)
+(* 5 6)
+(* 7 8)
