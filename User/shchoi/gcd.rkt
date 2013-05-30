ccae91ae3c18cd605dfe17fed6f117ead2db7bcf
최상훈 <shchoi@diotek.co.kr>
2013-05-30 23:32:00 +0900

    1.25

---
+++ User/shchoi/gcd.rkt
@@ -0,0 +1,27 @@
+#!/usr/bin/racket
+#lang planet neil/sicp
+(define (square x) (* x x))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+          (remainder (* base (expmod base (- exp 1) m))
+                     m))))
+
+(define (fast-expmod base exp m)
+  (remainder (fast-expt base exp) m))
+
+(expmod 3 7 100)
+(fast-expmod 3 7 100)
+
